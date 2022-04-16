5fac5557227b03c6a5d2472dcb5ac59ce49f650a
nootatowa <kyodo152015@gmail.com>
2022-04-16 05:50:17 +0000

    addコントローラ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,6 +17,37 @@ class Public::CustomersController < ApplicationController
       render :edit
     end
     end
+
+    def levelUp
+      customer = Customer.find(params[:id])
+      task = Task.find(params[:task_id])
+      #この二行は適当なものに置き換えてください。
+
+      totalExp = customer.experience_point
+      #変数に現在のユーザーの経験値を入れる
+      totalExp += monster.point
+      # 得られた経験値をユーザーの経験値に加算
+
+      customer.experience_point = totalExp
+      #改めて、加算した経験値をuserの総経験値を示す変数に入れ直す
+      customer.update(experience_point: totalExp)
+      #更新の処理をさせる
+
+      levelSetting = LevelSetting.find_by(level: customer.level + 1);
+      #ここから重要です！レベルセッティングのモデルから、今の自分のレベルより1高いレコードを探させます。
+      #そしてそれを変数に入れておきます
+
+      if levelSetting.thresold <= user.experience_point
+      #探してきたレコードの閾値よりもユーザーの総経験値が高かった場合
+        customer.level = customer.level + 1
+        customer.update(level: customer.level)
+      #レベルを1増やして更新
+      end
+    end
+
+
+
+
    private
     def customer_params
     params.require(:customer).permit(:name)
