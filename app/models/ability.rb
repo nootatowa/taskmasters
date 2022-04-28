8fea0b55227d01048d396d65dc544a1c1164b48b
nootatowa <kyodo152015@gmail.com>
2022-04-28 11:00:20 +0000

    ability commentout

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class Ability
+# class Ability
   # include CanCan::Ability
 
-  def initialize(user)
+  # def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
@@ -30,5 +30,5 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  end
-end
+#   end
+# end
