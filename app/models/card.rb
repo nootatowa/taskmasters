75fd394a0ee751e61f99fa9553470c0cba9ce2ef
nootatowa <kyodo152015@gmail.com>
2022-05-12 01:55:14 +0000

    file

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,12 +2,9 @@ class Card < ApplicationRecord
   belongs_to :task
   validates :title, length: { in: 1..255 }
   validates :memo,  length: { maximum: 1000 }
-  
-  
-  
-def written_by?(current_customer)
-   current_customer.id === task.customer.id
-end
 
+def written_by?(current_customer)
+    current_customer.id === task.customer.id
+end
 
 end
